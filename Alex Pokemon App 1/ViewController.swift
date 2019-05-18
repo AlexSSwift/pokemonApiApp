58fa252e116797fa49c7c29dc3553b7efe9df125
Alexander Smith <AlexSSwift@gmail.com>
2019-05-18 14:54:47 -0700

    Initial Commit

---
+++ Alex Pokemon App 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alex Pokemon App 1
+//
+//  Created by Austin Zheng on 5/18/19.
+//  Copyright © 2019 Austin Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
